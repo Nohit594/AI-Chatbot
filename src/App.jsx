2ea4128bc9208ae503a7aebcf1ab8c0a1fb901ad
Nohit Singh Chouhan <nohitsinghchouhan594@gmail.com>
2025-09-24 12:58:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Assistant } from "./assistants/googleai";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
+import styles from "./App.module.css";
+
+function App() {
+  const assistant = new Assistant();
+  const [messages, setMessages] = useState([]);
+
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await assistant.chat(content);
+      addMessage({ content: result, role: "assistant" });
+    } catch (error) {
+      addMessage({
+        content: "Sorry, I couldn't process your request. Please try again!",
+        role: "system",
+      });
+    }
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls onSend={handleContentSend} />
+    </div>
+  );
+}
+
+export default App;
